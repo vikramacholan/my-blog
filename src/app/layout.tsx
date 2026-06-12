750aa9b9cb0587ed394517bb5d8ce8c97bd2ca83
vikramacholan.aexp <vikramacholan.aexp@hotmail.com>
2026-06-13 02:42:57 +0530

    Add Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import BlogSidebar from "@/components/BlogSidebar";
 import "./globals.css";
 
@@ -37,6 +38,7 @@ export default function RootLayout({
           {/* Sidebar */}
           <BlogSidebar />
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
